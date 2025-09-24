bf9f33133a909e9a7991927adb6850cea54c633a
prgmr99 <johnyeom24@naver.com>
2025-09-24 17:45:47 +0900

    update readme

--- readMeUpdate.js
+++ readMeUpdate.js
@@ -42,7 +42,7 @@ const parser = new Parser({
 
       const date = dayjs(pubDate).add(9, "hours").format("YYYY.MM.DD");
       recentPostsText += `- <a href=${link}>${title}</a>\n`;
-      recentPostsText += `<sub>createdAt : ${date}</sub></br></br>\n`;
+      recentPostsText += `<sub>createdAt : ${date}</sub></br>\n`;
     }
 
     // 기존 내용 + Recent Posts 합치기
